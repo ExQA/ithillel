f45618780fb27a65bebb4d6de36c3d39f25529af
denis.svetlichnyi <denis.svetlichnyi@ringteam.com>
2023-10-06 18:57:21 +0300

    lesson-4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        GeometricShape circle = new Circle(5.0);
+        System.out.println("Площадь круга: " + circle.area());
     }
 }
