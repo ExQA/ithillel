3862ca083759bc1efc991923f0e0a2fc86c9e2c2
denis.svetlichnyi <denis.svetlichnyi@ringteam.com>
2023-10-23 23:42:00 +0300

    Homework 6 (ДЗ 7. Reversed List)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
 
+        ListNode reversed = reverseList;
+
+        while (reversed != null) {
+            System.out.print(reversed.val + " ");
+            reversed = reversed.next;
+        }
     }
 }
