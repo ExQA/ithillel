4f443f544a47b1e3b20cad1c73739a8c7a618b0d
denis.svetlichnyi <denis.svetlichnyi@ringteam.com>
2023-11-12 22:28:03 +0200

    ДЗ 10. Stream API

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
+
+        List<Product> products = new ArrayList<>();
+        products.add(new Product(1, "Book", 100, false, LocalDate.of(2022, 1, 1)));
+        products.add(new Product(2, "Book", 200, true, LocalDate.of(2022, 2, 1)));
+        products.add(new Product(3, "Toy", 100, false, LocalDate.of(2022, 10, 1)));
+        products.add(new Product(4, "Toy", 200, true, LocalDate.of(2022, 11, 1)));
+
+        Map<String, List<Product>> groupedProducts = FilterManager.groupByType(products);
+        List<Product> findByPrice = FilterManager.FilteredByPrice(100);
+
+        System.out.println(groupedProducts);
+        System.out.println(findByPrice);
 
     }
 }
---
+++ src/Product.java
@@ -0,0 +1,56 @@
+import javax.xml.crypto.Data;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+
+public class Product {
+    private final int id;
+    private final String type;
+    private double price;
+    private LocalDate createDate;
+    private boolean discountAvailable;
+
+    public Product(int id, String type, double price, boolean discountAvailable, LocalDate createDate) {
+        this.id = id;
+        this.type = type;
+        this.price = price;
+        this.discountAvailable = discountAvailable;
+        this.createDate = createDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public LocalDate getCreateDate() {
+        return createDate;
+    }
+
+    public boolean isDiscountAvailable() {
+        return discountAvailable;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id='" + id + '\'' +
+                ", type='" + type + '\'' +
+                ", price=" + price +
+                ", created=" + createDate +
+                ", discountAvailable=" + discountAvailable +
+                ", products=" +
+                '}';
+    }
+}
